663319de462e4e19f3d2f7640bd4d3b4840bf92c
xMaru32x <ma.amaya.332@gmail.com>
2025-01-01 00:39:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.querySelector('.heart').addEventListener('click', async function() {
 });
 
 function updateCountdown() {
-    let countDownDate = new Date('2025-01-01T00:00:00').getTime();
+    let countDownDate = new Date('2025-01-03T00:00:00').getTime();
     const now = Date.now();
     const distance = countDownDate - now;
 
